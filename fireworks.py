684df7da02c5215917ddb41d5cb466c645ec7d1c
Kevin Olson <olsonk@gmail.com>
2017-07-04 20:48:58 -0700

    Fixed code for fireworks

--- fireworks.py
+++ fireworks.py
@@ -1,29 +1,29 @@
-import time
-import random
+from time import sleep
+from random import randint
 from sense_hat import SenseHat
 
 class Firework():
 	
 	def __init__(self):
-		self.pos = [random.randint(1,6), 0]
-		self.color = [255, 255, 255]
+		self.pos = [randint(1,6), 7]
+		self.color = (255, 255, 255)
 		self.vert_speed = 1
 		self.horz_speed = 0
 		self.explode = False
 		
 	def animate(self):
-		if self.pos[1] < 4:
+		if self.pos[1] > 4:
 			self.pos[0] = self.pos[0] + self.horz_speed
-			self.pos[1] = self.pos[1] + self.vert_speed
+			self.pos[1] = self.pos[1] - self.vert_speed
 		else:
 			if not self.explode:
-				self.burst = Burst(self.pos[:], self.color[:])
+				self.burst = Burst(self.pos)
 				self.explode = True
 			self.burst.animate()
 		
 	def render(self, sensehat):
 		if not self.explode:
-			sensehat.set_pixel(self.pos[0], self.pos[1], self.color[0], self.color[1], self.color[2])
+			sensehat.set_pixel(self.pos[0], self.pos[1], self.color)
 		else:
 			self.burst.render(sensehat)
 
@@ -31,16 +31,14 @@ class Burst():
 
 	def __init__(self, pos):
 		self.pos = pos
-		self.color = [random.choice([0,255]), random.choice([0,255]), random.choice([0,255])]
-		self.vert_speed = 0
-		self.horz_speed = 0
+		self.color = [randint(100,255), randint(100,255), randint(100,255)]
 		self.particle_count = 10;
 		self.particle = []
 		self.create_particles();
 		
 	def create_particles(self):
 		for i in range(0, self.particle_count):
-			self.particle.append(Particle(self.pos[:], self.color[:]))
+			self.particle.append(Particle(self.pos, self.color))
 		
 	def animate(self):
 		for i in range(0, self.particle_count):
@@ -55,11 +53,8 @@ class Particle():
 	def __init__(self, pos, color):
 		self.pos = pos
 		self.color = color
-		self.vert_speed = random.randint(-1,0,1)
-		if self.vert_speed == 0:
-			self.horz_speed = random.randint(-1,1)
-		else:
-			self.horz_speed = random.randint(-1,0,1)
+		self.vert_speed = randint(-1,1)
+		self.horz_speed = randint(-1,1)
 	def animate(self):
 		self.pos[0] = self.pos[0] + self.horz_speed
 		self.pos[1] = self.pos[1] + self.vert_speed
@@ -69,11 +64,10 @@ class Particle():
 			return
 		sensehat.set_pixel(self.pos[0], self.pos[1], self.color[0], self.color[1], self.color[2])
 		
-		
 sense = SenseHat()
 fw = []
 fw_count = 9
-frame_count = 0
+frame_count = 20
 
 for i in range(0, fw_count):
 	fw.append(Firework())
@@ -81,9 +75,8 @@ for i in range(0, fw_count):
 while True:
 	sense.clear()
 	for i in range(0, fw_count):
-		if (frame_count > i * 10) {
+		if (frame_count > i * 10):
 			fw[i].animate()
 			fw[i].render(sense)
-		}
-	time.sleep(0.35)
+	sleep(0.35)
 	
---
+++ test.py
@@ -0,0 +1,5 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+sense.show_message("Test", text_colour=[255, 0,  0])
